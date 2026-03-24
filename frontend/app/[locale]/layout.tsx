d0b578c7361ea920d1f674347466221ec25cb2d4
Doan Tran Quy <quydoantran@gmail.com>
2026-03-23 20:59:52 -0700

    chore: remove empty metadata export from locale layout

--- frontend/app/[locale]/layout.tsx
+++ frontend/app/[locale]/layout.tsx
@@ -1,10 +1,7 @@
-import type { Metadata } from 'next';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
-
-export const metadata: Metadata = {};
 
 export default async function LocaleLayout({
   children,
